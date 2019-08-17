fb103f06ddcd35db6b9776122c76a1cab289c087
Yesid <Yesid@ASUS.lan>
2019-08-17 10:40:23 -0500

    Agregamos atributos a la clase usuario

---
+++ FormularioUsuario/src/main/java/a/com/modelo/Usuario.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package a.com.modelo;
+
+/**
+ *
+ * @author Yesid
+ */
+public class Usuario {
+    private String nombre;
+    private double salario;
+    private int dias;
+    private String idioma[];
+    
+}
